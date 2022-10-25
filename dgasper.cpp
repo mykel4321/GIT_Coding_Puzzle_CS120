7e804add86cb302f07d50362b4e3ec0f9825e9e7
Dom Gasperini <38026244+dom-gasperini@users.noreply.github.com>
2022-10-25 10:58:29 -0400

    Create dgasper.cpp

---
+++ dgasper.cpp
@@ -0,0 +1,19 @@
+
+#include <vector>
+#include <string>
+
+using namespace std
+int main()
+{
+  vector<int> name = {01000100, 01101111, 01101101};
+  vector<int> section = {01000011, 01010011, 00100000, 00110001, 00110010, 00110000}; 
+  printf("%s", [] (std::vector<int> name, std::vector section) -> {
+    string result = "";
+    for (int i = 0; i < name.size() - 1; ++i) 
+      result += (std::bitset<8>(vec.at(i)).to_string();
+    for (int i = 0; i < section.size() - 1; ++i) 
+      result += (std::bitset<8>(vec.at(i)).to_string(); return result;
+  });
+  
+  return 0;
+}
