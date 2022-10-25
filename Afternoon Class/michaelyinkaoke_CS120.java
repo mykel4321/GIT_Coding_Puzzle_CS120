651356ea92679b8a5aff8f6c6ba7a255b1c2bb99
mykel4321 <112426492+mykel4321@users.noreply.github.com>
2022-10-25 13:43:47 -0400

    Create michaelyinkaoke_CS120.java

---
+++ Afternoon Class/michaelyinkaoke_CS120.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Main {
+  public static void main(String[] args) {
+    ArrayList<String> list = new ArrayList<String>();
+    list.add("A");
+    list.add(1);
+    list.add("B");
+    list.add(2);
+    list.add("C");
+    list.add(3);
+    list.add("D");
+    list.add(4);
+    System.out.println(cars);
+  }
+}
